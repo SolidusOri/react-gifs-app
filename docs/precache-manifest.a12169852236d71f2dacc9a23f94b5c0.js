40dc104655450b1a38d479ef96320632ae3869c2
Solidus <robinsonallendes@hotmail.com>
2020-08-01 00:45:44 -0400

    se cambia nombre a carpeta build a docs

---
+++ docs/precache-manifest.a12169852236d71f2dacc9a23f94b5c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d914360f31ea58985ecff898c3cf3948",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0414e9b9a6d482e4bdc9",
+    "url": "/static/css/main.0d2e8a99.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0414e9b9a6d482e4bdc9",
+    "url": "/static/js/main.13ead7d9.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
